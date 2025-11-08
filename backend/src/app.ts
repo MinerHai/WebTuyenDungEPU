146b6491f4fca695879d38ddfb69edb4ab32317e
MinerHai <haiminer45@gmail.com>
2025-11-08 22:53:12 +0700

    .
    .

---
+++ backend/src/app.ts
@@ -0,0 +1,13 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import connectDB from "./config/db";
+
+const app = express();
+app.use(express.json());
+
+connectDB();
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => console.log(`Server running on port ${PORT}`));
---
+++ backend/src/config/db.ts
@@ -0,0 +1,16 @@
+import mongoose from "mongoose";
+
+const connectDB = async () => {
+  try {
+    console.log(process.env.CONNECT_DATABASE_URL);
+    await mongoose.connect(
+      process.env.CONNECT_DATABASE_URL?.toString() || "",
+      {}
+    );
+    console.log("MongoDB connected");
+  } catch (error) {
+    console.error("MongoDB connection error:", error);
+  }
+};
+
+export default connectDB;
